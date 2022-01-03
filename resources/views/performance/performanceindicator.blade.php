18558cda227eb298db0214091fc76f1a4ec9df2c
Soeng Souy <soengsouy@gmail.com>
2022-01-04 05:54:40 +0700

    remove side someone

--- resources/views/performance/performanceindicator.blade.php
+++ resources/views/performance/performanceindicator.blade.php
@@ -129,9 +129,6 @@
                             <li><a href="training-type.html"> Training Type </a></li>
                         </ul>
                     </li>
-                    <li><a href="promotion.html"><i class="la la-bullhorn"></i> <span>Promotion</span></a></li>
-                    <li><a href="resignation.html"><i class="la la-external-link-square"></i> <span>Resignation</span></a></li>
-                    <li><a href="termination.html"><i class="la la-times-circle"></i> <span>Termination</span></a></li>
                     <li class="menu-title"> <span>Administration</span> </li>
                     <li> <a href="assets.html"><i class="la la-object-ungroup">
                         </i> <span>Assets</span></a>
